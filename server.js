430a1baf1fcfad0e488e42841307afd3446b47ba
gul hossain <spicymangojungle@gmail.com>
2020-04-08 22:35:59 -0700

    fixed server

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+server.use(logger);
+
+//router
+const userRouter = require("./users/userRouter");
+server.use("/api/users", userRouter);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method, req.url, Date.now());
+  next();
+}
 
 module.exports = server;
